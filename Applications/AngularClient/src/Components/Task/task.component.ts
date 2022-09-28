650f90178ea0fa83523fd2b957ad4af23fca55d9
engin özdemir <xenamorphx@gmail.com>
2022-09-28 18:10:14 +0300

    Initial commit

---
+++ Applications/AngularClient/src/Components/Task/task.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-task',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css']
+})
+export class TaskComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
